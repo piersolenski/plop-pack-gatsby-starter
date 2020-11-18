48ee5e0efa235cdf47c4b83d26095e333082678e
Piers Olenski <hello@piers.io>
2020-11-18 21:14:51 +0000

    Update order of plop options

--- plopfile.js
+++ plopfile.js
@@ -1,10 +1,10 @@
 module.exports = (plop) => {
   plop.load([
-    './src/generators/bootstrap.js',
     './src/generators/component.js',
-    './src/generators/icon.js',
-    './src/generators/hook.js',
     './src/generators/page.js',
+    './src/generators/hook.js',
+    './src/generators/icon.js',
+    './src/generators/bootstrap.js',
     './src/generators/sanity.js',
   ]);
 };
